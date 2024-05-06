dc9de662ed170ff2914f5cc767ea4b3372777c5f
cakeller2 <cakeller2@quinnipiac.edu>
2024-05-06 07:47:42 -0400

    updates to SignUp/index.js

--- e-comerce-frontend/src/screens/SignUp/index.js
+++ e-comerce-frontend/src/screens/SignUp/index.js
@@ -42,39 +42,39 @@ function Index() {
             }}
           >
             <div style={{cursor: 'pointer'}} onClick={() => push('/')}>
-              <i class="fas fa-arrow-circle-left fa-5x"></i>
+              <i className="fas fa-arrow-circle-left fa-5x"></i>
             </div>
             <p>Signup</p>
           </div>
 
-          <label for="fname">Full Name</label>
+          <label htmlFor="fname">
           <input
             type="text"
             id="fname"
             name="firstname"
             placeholder="Your full name.."
             value={fullName}
-            onChange={e => setFullName(e.target.value)}
-          />
+            onChange={e => setFullName(e.target.value)}/>
+            Full Name</label>
 
-          <label for="email">Email</label>
+          <label htmlFor="email">
           <input
             type="email"
             id="lname"
             name="email"
             placeholder="Your email.."
             value={email}
-            onChange={e => setEmail(e.target.value)}
-          />
-          <label for="password">Password</label>
+            onChange={e => setEmail(e.target.value)}/>
+            Email</label>
+          <label for="password">
           <input
             type="password"
             id="lname"
             name="password"
             placeholder="Your Password.."
             value={password}
-            onChange={e => setPassword(e.target.value)}
-          />
+            onChange={e => setPassword(e.target.value)}/>
+            Password</label>
 
           <Link to="/signin" className="link">
             <span>Already have an account ?</span>

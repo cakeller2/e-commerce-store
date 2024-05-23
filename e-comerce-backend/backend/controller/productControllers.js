322cd9b2fa006f2d52ef3bed8b48689bf55247bd
cakeller2 <cakeller2@quinnipiac.edu>
2024-05-23 19:45:25 -0400

    working out issues

--- e-comerce-backend/backend/controller/productControllers.js
+++ e-comerce-backend/backend/controller/productControllers.js
@@ -1,6 +1,7 @@
 const Product = require("../models/Product");
 const products = [
   {
+    id: "1",
     name: "PlayStation 5",
     imageUrl:
       "https://images.unsplash.com/photo-1606813907291-d86efa9b94db?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80",
@@ -10,6 +11,7 @@ const products = [
     countInStock: 15,
   },
   {
+    id: "2",
     name: "Iphone 12",
     imageUrl:
       "https://images.unsplash.com/photo-1605787020600-b9ebd5df1d07?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1463&q=80",
@@ -19,6 +21,7 @@ const products = [
     countInStock: 10,
   },
   {
+    id: "3",
     name: "Cannon EOS-1D",
     imageUrl:
       "https://images.unsplash.com/photo-1519183071298-a2962feb14f4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
@@ -28,6 +31,7 @@ const products = [
     countInStock: 5,
   },
   {
+    id: "4",
     name: "Amazon Alexa",
     imageUrl:
       "https://images.unsplash.com/photo-1518444065439-e933c06ce9cd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80",
@@ -37,6 +41,7 @@ const products = [
     countInStock: 25,
   },
   {
+    id: "5",
     name: "Audio Technica Headphones",
     imageUrl:
       "https://images.unsplash.com/photo-1558756520-22cfe5d382ca?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
@@ -46,6 +51,7 @@ const products = [
     countInStock: 4,
   },
   {
+    id: "6",
     name: "JBL FLIP 4",
     imageUrl:
       "https://images.unsplash.com/photo-1564424224827-cd24b8915874?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1489&q=80",
@@ -60,7 +66,7 @@ const products = [
 
 const getProducts = async (req, res) => {
   try {
-    const productsFromDB = await Product.find({});
+    //const productsFromDB = await Product.find({});
     res.json(products);
   } catch (error) {
     console.error(error);
@@ -70,14 +76,18 @@ const getProducts = async (req, res) => {
 
 const getProductById = async (req, res) => {
   try {
-   // const product = await Product.findById(req.params.id);
-
-   // res.json(product);
+    // const product = await Product.findById(req.params.id);
+    const product = findElementByProperty(products, 'id', req.params.id);
+    res.json(product);
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Server Error" });
   }
 };
+
+function findElementByProperty(array, property, value) {
+  return array.find(element => element[property] === value);
+}
 
 module.exports = {
   getProducts,
